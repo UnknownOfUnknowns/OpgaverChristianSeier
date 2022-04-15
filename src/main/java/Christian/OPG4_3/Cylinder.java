ef43f3520b929e9b23c8516fa8f7a9a12353432e
Christian <s215703@dtu.dk>
2022-04-15 10:34:55 +0200

    Ny Setter and Getter

--- src/main/java/Christian/OPG4_3/Cylinder.java
+++ src/main/java/Christian/OPG4_3/Cylinder.java
@@ -1,4 +1,27 @@
 package Christian.OPG4_3;
 
 public class Cylinder {
+
+    private double radius;
+    private double højde;
+
+     public float Cylinder (double radius, double højde) {
+
+        public double getHøjde() {
+            return (float) højde;
+        }
+
+        public int getRadius() {
+            return (float) radius;
+        }
+
+        public void setHøjde(højde) {
+             this.højde = 0.0;
+         }
+
+         public void setRadius(radius) {
+            this.radius;
+        }
+    }
+
 }
