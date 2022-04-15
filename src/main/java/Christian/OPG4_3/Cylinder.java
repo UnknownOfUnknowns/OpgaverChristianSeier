a4f247c5fe0c1a9426eb430f9eccb2c34497d382
Seier K <seierk@gmail.com>
2022-04-15 11:33:10 +0200

    Changes by hans

--- src/main/java/Christian/OPG4_3/Cylinder.java
+++ src/main/java/Christian/OPG4_3/Cylinder.java
@@ -5,26 +5,6 @@ public class Cylinder {
     private double radius;
     private double højde;
 
-     public float Cylinder (double radius, double højde) {
 
-        public double getHøjde() {
-            return (float) højde;
-        }
-
-        public int getRadius() {
-            return (float) radius;
-        }
-
-        public void setHøjde(højde) {
-             this.højde = 0.0;
-         }
-
-         public void setRadius(radius) {
-            this.radius;
-        }
-
-        VIRKER IKKE
-
-    }
 
 }
