5a7fdb02ff63c0d423561799c5caed5949ed7c06
Christian <s215703@dtu.dk>
2022-04-15 11:15:20 +0200

    tilføjet Main class, samt få getters og setters.

--- src/main/java/Christian/OPG4_3/Cylinder.java
+++ src/main/java/Christian/OPG4_3/Cylinder.java
@@ -5,26 +5,43 @@ public class Cylinder {
     private double radius;
     private double højde;
 
-     public float Cylinder (double radius, double højde) {
 
-        public double getHøjde() {
-            return (float) højde;
-        }
-
-        public int getRadius() {
-            return (float) radius;
-        }
-
-        public void setHøjde(højde) {
-             this.højde = 0.0;
-         }
-
-         public void setRadius(radius) {
-            this.radius;
-        }
-
-        VIRKER IKKE
-
+    public Cylinder (double r, double h) {
+        this.radius = r;
+        this.højde = h;
     }
 
+
+    public double getHøjde() {
+        return højde;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setHøjde(double højde) {
+        this.højde = højde;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getVolume() {
+    return Math.PI*Math.pow(radius, 2)*højde;
+    }
+
+    public double getAreal() {
+        return 2*Math.PI*radius*højde+2*Math.PI*Math.pow(radius, 2);
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder{" +
+                "radius=" + radius +
+                ", højde=" + højde +
+                ", areal=" + getAreal() +
+                '}';
+    }
 }
