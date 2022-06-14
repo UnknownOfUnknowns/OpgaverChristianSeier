24fee46a71c0e4c860412286986c997981d07bfe
Christian <s215703@dtu.dk>
2022-06-14 21:33:18 +0200

    nyopgaver7.1-7.2-7.3

--- src/main/java/Christian/OPG7_2/Sales.java
+++ src/main/java/Christian/OPG7_2/Sales.java
@@ -3,8 +3,10 @@ package Christian.OPG7_2;
 public class Sales {
     public static void main(String[] args) {
         SalesPerson pers1 = new SalesPerson("Christian", "22422112","Rødovre");
-
+        pers1.setDailyAmount(1, 500000);
         System.out.println(pers1);
 
+        pers1.setDailyAmount(2, 120);
+        System.out.println(pers1);
     }
 }
--- src/main/java/Christian/OPG7_2/SalesPerson.java
+++ src/main/java/Christian/OPG7_2/SalesPerson.java
@@ -4,38 +4,48 @@ package Christian.OPG7_2;
 
 import java.time.DayOfWeek;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SalesPerson {
 
     private String name;
     private String phonenr;
     private String district;
-    private double sale_amount, DailyAmount;
+    private double[] sale_amount;
 
 
 
+    public SalesPerson(String name, String phonenr, String district, double[] sale_amount) {
+        this.name = name;
+        this.phonenr = phonenr;
+        this.district = district;
+        this.sale_amount = sale_amount;
+    }
 
     public SalesPerson(String name, String phonenr, String district) {
         this.name = name;
         this.phonenr = phonenr;
         this.district = district;
-    }
-    public SalesPerson(String name, double sale_amount) {
-        this.name = name;
-        this.sale_amount = sale_amount;
-
+        this.sale_amount = new double[7];
     }
 
-    public enum DayOfTheWeek {
-        Sunday,
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday
+    public void setDailyAmount(int day, double amount) {
+        sale_amount[day] = amount;
     }
 
 
+
+
+
+
+    @Override
+    public String toString() {
+        return "SalesPerson{" +
+                "name='" + name + '\'' +
+                ", phonenr='" + phonenr + '\'' +
+                ", district='" + district + '\'' +
+                ", sale_amount=" + Arrays.toString(sale_amount) +
+                '}';
+    }
 }
 
