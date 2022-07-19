5560c6394a5232f27757a36d7f9aee884c6f5577
Seier K <seierk@gmail.com>
2022-07-19 10:46:44 +0200

    opg opdateret

---
+++ src/main/java/Seier/OPG8_10/Question.java
@@ -0,0 +1,34 @@
+package Seier.OPG8_10;
+
+public class Question implements Complexity {
+    private String question, answer;
+    private int complexitylevel;
+
+    public Question(String query, String result) {
+        question = query;
+        answer = result;
+        complexitylevel = 1;
+    }
+
+    public void setComplexity(int level) {
+        complexitylevel = level;
+    }
+
+    public int getComplexity() {
+        return complexitylevel;
+    }
+    public String getQuestion(){
+        return question;
+    }
+    public String getAnswer(){
+        return answer;
+    }
+    public boolean answerCorrect(String candidateAnswer) {
+        return answer.equals(candidateAnswer);
+    }
+
+    public String toString() {
+        return question + "\n" + answer;
+    }
+}
+
---
+++ src/main/java/Seier/OPG8_10/Quiz.java
@@ -0,0 +1,32 @@
+package Seier.OPG8_10;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Quiz {
+    ArrayList<Question> questions = new ArrayList<>();
+
+    public Quiz() {
+
+    }
+
+    public void questionsadd(Question question){
+        questions.add(question);
+    }
+    public void giveQuiz (){
+        int point = 0;
+        for(int i = 0; i < questions.size(); i++) {
+            System.out.println(questions.get(i).getQuestion());
+
+            Scanner scan = new Scanner(System.in);
+            String answer = scan.next();
+            if(questions.get(i).answerCorrect(answer)){
+                point += 1;
+            }
+            System.out.println("point efter denne runde " + point);
+        }
+        System.out.println("spilleren har: " + point + " Point" + " ud af " + questions.size() + " spørgsmål");
+
+        }
+    }
+
