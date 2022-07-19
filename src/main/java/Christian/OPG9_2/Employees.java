eea61e47e411bdf599911cb0d938ea2b700984df
Christian <s215703@dtu.dk>
2022-07-19 12:22:30 +0200

    Inheritance opg 9.2

---
+++ src/main/java/Christian/OPG9_2/Employees.java
@@ -0,0 +1,11 @@
+package Christian.OPG9_2;
+
+public class Employees {
+    String name;
+    String title;
+
+    public Employees(String name, String title) {
+        this.name = name;
+        this.title = title;
+    }
+}
---
+++ src/main/java/Christian/OPG9_2/Surgeon.java
@@ -0,0 +1,4 @@
+package Christian.OPG9_2;
+
+public class Surgeon {
+}
