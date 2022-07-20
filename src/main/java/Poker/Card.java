3f76c2570f3792d8a900f5a05b97e847abb380b1
Seier K <seierk@gmail.com>
2022-07-20 13:53:41 +0200

    pokerprojekt

--- src/main/java/Poker/Card.java
+++ src/main/java/Poker/Card.java
@@ -1,4 +1,13 @@
 package Poker;
 
 public class Card {
+
+    Suit suit;
+    Value value;
+
+    public Card(Suit suit,Value value){
+        this.suit = suit;
+        this.value = value;
+
+    }
 }
--- src/main/java/Poker/Game.java
+++ src/main/java/Poker/Game.java
@@ -1,4 +1,11 @@
 package Poker;
 
 public class Game {
+
+    public static void main(String[] args) {
+
+        public game(){
+
+        }
+    }
 }
